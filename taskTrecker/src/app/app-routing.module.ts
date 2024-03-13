13bb4698fd6aeae804159d8e4efd8dbbc4c387c2
Zhadi <zhadi000s@gmail.com>
2024-03-13 13:26:09 +0600

    Сделал верстку и устновил bootstrap

--- taskTrecker/src/app/app-routing.module.ts
+++ taskTrecker/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
+import { AllTasksComponent } from './components/all-tasks/all-tasks.component';
+import { CreateTaskComponent } from './components/create-task/create-task.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'main',pathMatch:'full'},
-  {path:'main',component:MainComponent}
+  {path:'main',component:MainComponent},
+  {path:'create',component:CreateTaskComponent},
+  {path:'tasks',component:AllTasksComponent}
 ];
 
 @NgModule({
